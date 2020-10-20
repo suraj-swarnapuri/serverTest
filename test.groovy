108c9e42b593e9398d738fec54ea2a1c8282e6db
surajswarnapuri <suraj.swarnapuri@ibm.com>
2020-10-20 13:09:13 -0500

    test groovy

--- test.groovy
+++ test.groovy
@@ -1,17 +1,12 @@
 pipeline{
    agent any
-   parameters{
-     string(name:'VAR', defaultValue:'',description:'test var')
-
-
-}
   stages{
    stage("test"){
     steps{
 
 
     sh''' #!/bin/bash
-	echo ${params.VAR}
+	echo ${params.test_choice}
 '''
 
 }
